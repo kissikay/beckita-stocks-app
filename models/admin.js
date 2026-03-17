1705793ff207b03645ee52aa4a8619d66e207eef
Kissi Stanley Kofi <stanleykissi7@gmail.com>
2026-03-16 17:10:59 -0700

    fix: mongoose pre-save hook TypeError and update to standard Atlas URI

--- models/admin.js
+++ models/admin.js
@@ -31,15 +31,11 @@ const adminSchema=new Schema({
 }, { timestamps: true })
 
 // Hash password before saving
-adminSchema.pre("save", async function(next) {
-    if (!this.isModified("password")) return next();
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (error) {
-        next(error);
-    }
+adminSchema.pre("save", async function() {
+    if (!this.isModified("password")) return;
+    
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Method to compare password
